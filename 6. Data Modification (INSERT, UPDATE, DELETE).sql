e4ea375386b28709954f45461bd9ff14b4b1a2bf
Olalekan Asenuga <83604839+Rhydee@users.noreply.github.com>
2024-10-07 08:41:22 +0100

    Update 6. Data Modification (INSERT, UPDATE, DELETE).sql

--- 6. Data Modification (INSERT, UPDATE, DELETE).sql
+++ 6. Data Modification (INSERT, UPDATE, DELETE).sql
@@ -1,5 +1,9 @@
-select * from orders;
+--- Business Problem 1: Promotional Discounts(Apply a 9% discount to all products in a specific category (e.g., "electronics").)
 
-
-UPDATE orders
-SET order_estimated_delivery_date = DATE(order_estimated_delivery_date);
+update order_items
+join products 
+on products.product_id = order_items.product_id
+join payments
+on payments.order_id = order_items.order_id
+SET order_items.price = order_items.price * 0.09
+WHERE products.product_category_name = 'electronics';
